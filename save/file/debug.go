1d671ecbeb3a6f84f3aa20d731828c54b2b06b6d
Travis Offtermatt <travisty2@gmail.com>
2024-06-23 09:41:41 -0600

    pretty json

--- save/file/debug.go
+++ save/file/debug.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"encoding/json"
 	"os"
 	"strings"
 )
@@ -8,8 +9,15 @@ import (
 func printFile(name string, b []byte) {
 	if os.Getenv("PR_PRINT") == "true" {
 		s := strings.ReplaceAll(string(b), "\\", "")
-		s = strings.ReplaceAll(s, "\"{", "\n{")
+		s = strings.ReplaceAll(s, "\"{", "{")
 		s = strings.ReplaceAll(s, "}\"", "}")
-		os.WriteFile(name, []byte(s), 0755)
+		var m map[string]any
+		if err := json.Unmarshal([]byte(s), &m); err == nil {
+			if b, err = json.MarshalIndent(m, "", "\t"); err == nil {
+				_ = os.WriteFile(name, b, 0755)
+				return
+			}
+		}
+		_ = os.WriteFile(name, []byte(s), 0755)
 	}
 }
